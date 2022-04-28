0b3152fc695fc1b6dfad4ddb8c8d3bd3c892ead7
zuri227 <zuri22780@gmail.com>
2022-04-28 14:42:01 -0400

    Create basic header bar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="header__logo">
+
+      </div>
+
+      <div className="header__search">
+
+      </div>
+      <div className="header__nav">
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Hello Guest</span>
+          <span className="nav__itemLineTwo">Sign In</span>
+        </div>
+
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Your</span>
+          <span className="nav__itemLineTwo">Shop</span>
+        </div>
+
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Basket</span>
+          <span className="nav__itemLineTwo">0</span>
+        </div>
+      </div>
+
+
     </div>
   );
 }
