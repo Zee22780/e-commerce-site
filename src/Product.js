c26e8181ccb925f7d52799b3c847d19c1cbc2383
zuri227 <zuri22780@gmail.com>
2022-04-28 22:14:13 -0400

    Add product details to Home component

--- src/Product.js
+++ src/Product.js
@@ -1,19 +1,24 @@
 import React from 'react'
+import './Product.css'
 
 function Product() {
   return (
-    <div>
       <div className="product">
         <div className="product__info">
           <p>Title</p>
-          <p className="product__price">$30</p>
-          <div className="product__rating">⭐️ ⭐️ ⭐️ </div>
+          <p className="product__price">
+            <small>$</small>
+            <strong>30</strong>
+          </p>
+          <div className="product__rating">
+            ⭐️ ⭐️ ⭐️ 
+          </div>
         </div>
-          <img src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg" alt="product-image" />
+          <img src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg" alt="" />
 
           <button>Add to Basket</button>
+      
       </div>
-    </div>
   )
 }
 
