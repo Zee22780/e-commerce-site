f2f39ff328cf8c2dfa58e0cbb5f249f64ce4ebb3
zuri227 <zuri22780@gmail.com>
2022-04-28 22:24:56 -0400

    Add deconstructed props to product.js

--- src/Product.js
+++ src/Product.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import './Product.css'
 
-function Product() {
+function Product({id, title, image, price, rating}) {
   return (
       <div className="product">
         <div className="product__info">
-          <p>Title</p>
+          <p>{title}</p>
           <p className="product__price">
             <small>$</small>
-            <strong>30</strong>
+            <strong>{price}</strong>
           </p>
           <div className="product__rating">
-            ⭐️ ⭐️ ⭐️ 
+            {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐️</p>
+            ))}
           </div>
         </div>
-          <img src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg" alt="" />
+          <img src={image} />
 
           <button>Add to Basket</button>
       
