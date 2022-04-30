c4e868b5226eecbf45f943e77f3c54f4b6238867
zuri227 <zuri22780@gmail.com>
2022-04-29 20:38:53 -0400

    Add remove from basket functionality

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import './CheckoutProduct.css'
+import { useStateValue } from './StateProvider'
 
 function CheckoutProduct({id, image, title, price, rating}) {
+  const [{basket}, dispatch] = useStateValue()
+
+  const removeFromBasket = () => {
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    })
+  }
+
   return (
     <div className="checkoutProduct">
         <img src={image} className="checkoutProduct__image"/>
@@ -19,7 +29,7 @@ function CheckoutProduct({id, image, title, price, rating}) {
                 <p>⭐️</p>
               ))}
             </div>
-            <button>Remove from Basket</button>
+            <button onClick={removeFromBasket}>Remove from Basket</button>
         </div>
       </div>
   )
--- src/Reducer.js
+++ src/Reducer.js
@@ -1,5 +1,7 @@
 //define application level states and define actions to make the changes to the state
 
+import { ProductionQuantityLimitsTwoTone } from "@mui/icons-material"
+
 export const initialState = {
   basket: [],
 }
@@ -14,10 +16,27 @@ export const getBasketTotal = (basket) => {
 const reducer = (state, action) => {
   switch(action.type) {
     case "ADD_TO_BASKET":
-    return {
+      return {
       ...state,
       basket: [...state.basket, action.item],
-    }
+      }
+    case "REMOVE_FROM_BASKET":
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      )
+      let newBasket = [...state.basket]
+      
+      if (index>=0) {
+        newBasket.splice(index, 1)
+      } else {
+        console.warn(
+          `Can't remove product(id: ${action.id}) as it's not in the basket`
+        )
+      }
+      return{
+        ...state,
+        basket: newBasket
+      }
     default:
       return state;
   }
