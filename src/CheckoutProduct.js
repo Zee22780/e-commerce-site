97aaed005022e5c3e6acd0614d70a7e9faffe472
zuri227 <zuri22780@gmail.com>
2022-04-29 20:12:38 -0400

    Add add to cart functionality

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import './CheckoutProduct.css'
 
-function CheckoutProduct() {
+function CheckoutProduct({id, image, title, price, rating}) {
   return (
     <div className="checkoutProduct">
-        <img src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg" alt="" className="checkoutProduct__image" />
-      <div>
+        <img src={image} className="checkoutProduct__image"/>
+      
         <div className="checkoutProduct__info">
-          <p className="checkoutProduct__title">Bennett Mystic 15.6 inch Laptop Shoulder Messenger Sling Office Bag, Water Repellent Fabric for Men and Women (Blue)</p>
+          <p className="checkoutProduct__title">{title}</p>
             <p className="checkoutProduct__price">
               <small>$</small>
-              <strong>20</strong>
+              <strong>{price}</strong>
             </p>
             <div className="checkoutProduct__rating">
-              ⭐️
+              {Array(rating)
+              .fill()
+              .map((_, i) => (
+                <p>⭐️</p>
+              ))}
             </div>
             <button>Remove from Basket</button>
         </div>
       </div>
-
-    </div>
   )
 }
 
