7c5083a6c254be20d94de065cee1007255629930
zuri227 <zuri22780@gmail.com>
2022-04-29 20:19:07 -0400

    Add subtotal component functionality

--- src/Reducer.js
+++ src/Reducer.js
@@ -5,6 +5,12 @@ export const initialState = {
 }
 
 //selector
+
+export const getBasketTotal = (basket) => {
+  return(basket?.reduce((amount, item) => item.price + amount, 0))
+}
+
+
 const reducer = (state, action) => {
   switch(action.type) {
     case "ADD_TO_BASKET":
